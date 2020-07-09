541c1701dca661887382b609745a08ad1bdb7995
Eric Chauvin <chauvineric4@gmail.com>
2020-07-09 12:51:58 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,48 @@
+// Copyright Eric Chauvin 2020.
+
+
+// #include <cstdlib>
+// #include <string>
+// #include <vector>
+#include <iostream>
+// #include <fstream>
+
+using namespace std;
+
+
+
+int main( void )
+{
+cout << "Testing this. "
+        "There is no plus sign here.\n";
+
+
+
+// bool TestThis = true;
+
+// void PassPointer( int* APointer )
+// void PassReference( int& AReference )
+// Use it like a regular variable.
+// AReference = 5;
+
+
+string TestS = "This and that.";
+TestS += " and this too.\n";
+cout << TestS;
+
+// Like a batch file to call a Java program to show it?
+// Like a FileViewer.exe.
+// Like a 3D Graphics viewer.
+// system( "executethisfile.bat" );
+
+
+/*
+ ifstream in("Scopy.cpp");  // Open for reading
+  ofstream out("Scopy2.cpp"); // Open for writing
+  string s;
+   while( getline(in, s) ) // Discards newline char
+     out << s << "\n"; /
+*/
+
+// vector<string> v;
+}
