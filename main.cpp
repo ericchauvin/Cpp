6dab5d17e0de9085336ecf1703ce48e98b82711d
Eric Chauvin <chauvineric4@gmail.com>
2020-07-10 09:52:05 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-// Copyright Eric Chauvin 2020.
+// A basic Hello World program needs to have a
+// lot of preprocessing done because of the #include
+// files.  This program is used to test preprocessing
+// on those #include files.
+
 
 
 // #include <cstdlib>
-// #include <string>
 // #include <vector>
 #include <iostream>
 // #include <fstream>
@@ -12,37 +15,8 @@ using namespace std;
 
 
 int main( void )
-{
-cout << "Testing this. "
-        "There is no plus sign here.\n";
+  {
+  cout << "A basic Hello "
+        "World program.\n";
 
-
-
-// bool TestThis = true;
-
-// void PassPointer( int* APointer )
-// void PassReference( int& AReference )
-// Use it like a regular variable.
-// AReference = 5;
-
-
-string TestS = "This and that.";
-TestS += " and this too.\n";
-cout << TestS;
-
-// Like a batch file to call a Java program to show it?
-// Like a FileViewer.exe.
-// Like a 3D Graphics viewer.
-// system( "executethisfile.bat" );
-
-
-/*
- ifstream in("Scopy.cpp");  // Open for reading
-  ofstream out("Scopy2.cpp"); // Open for writing
-  string s;
-   while( getline(in, s) ) // Discards newline char
-     out << s << "\n"; /
-*/
-
-// vector<string> v;
-}
+  }
