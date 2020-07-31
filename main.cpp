dc7bf989e52df665e02586616263a64157440392
Eric Chauvin <chauvineric4@gmail.com>
2020-07-31 11:10:48 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,17 @@
-// A basic Hello World program needs to have a
+
+
+// A very basic program like this needs to have a
 // lot of preprocessing done because of the #include
 // files.  This program is used to test preprocessing
 // on those #include files.
 
 
 
-// #include <cstdlib>
-// #include <vector>
-#include <iostream>
-// #include <fstream>
-
-using namespace std;
+#include <stdio.h>
 
 
 
 int main( void )
   {
-  cout << "A basic Hello "
-        "World program.\n";
-
+  printf( "This is a test.\n" );
   }
