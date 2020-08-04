e0bb16e982f188ff68979e711f40bde37067ed43
Eric Chauvin <chauvineric4@gmail.com>
2020-08-04 13:27:04 -0600

    Update RunMain.bat

--- RunMain.bat
+++ RunMain.bat
@@ -1,7 +1,10 @@
 @echo off
 echo This is RunMain.bat.
 
-SET PATH=%PATH%;\cygwin64\bin\;
+rem SET PATH=%PATH%;\cygwin64\bin\;
+
+rem mingw needs to find a libGcc or something.
+SET PATH=%PATH%;\mingw\bin\;
 
 cd \EricMain\Cpp
 \EricMain\Cpp\main.exe > ExeOut.txt
