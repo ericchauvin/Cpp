d6abf24020e4e9e08fee7f12511457501a83149f
Eric Chauvin <chauvineric4@gmail.com>
2020-08-06 12:54:59 -0600

    Create WindowsApiForCpp.h

---
+++ WindowsApiForCpp.h
@@ -0,0 +1,43 @@
+// Written by Eric Chauvin.
+
+
+
+// This is open source software based on the
+// GNU GENERAL PUBLIC LICENSE Version 2.
+
+
+#ifndef GuardMacroWindowsApiForCpp
+#define GuardMacroWindowsApiForCpp
+
+#include "TypeDefinitions.h"
+
+
+// typedef LONG HRESULT;
+
+// # define _EXTERN_C       extern "C"
+// # define _BEGIN_C_DECLS  extern "C" {
+// # define _END_C_DECLS    }
+
+// #define MAX_PATH 260
+// #define WINAPI __stdcall
+
+// WORD is Uint16.
+// DWORD is a Uint32.
+// BYTE is Uint8.
+
+
+// #define NULL 0
+// typedef int WINBOOL,*PWINBOOL,*LPWINBOOL;
+// typedef BOOL *PBOOL,*LPBOOL;
+
+
+
+
+
+
+
+#endif 
+
+
+
+ 
